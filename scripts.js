d39f1bd227eed055d657d15d6144bb9db2f47e39
Max Lane <mxlane87@gmail.com>
2023-06-15 09:19:09 -0400

    Testing global context

--- scripts.js
+++ scripts.js
@@ -78,7 +78,6 @@ function onFormSubmit(event){
           "name": "Developer",
           "parentGroupId": "TestingAttribute"
         });
-        aptrinsic('set', 'globalContext', {"version":12345, "name":"Salesforce", "type":"IFrame"});
       } else if (usernameInput.value === "mxwell.lane@gmail.com") {
         aptrinsic("identify",
         {
